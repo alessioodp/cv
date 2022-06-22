48a53987da18dcc063aa1e85329d99dc4d45f08a
AleAle <alessioodp@yahoo.it>
2022-06-22 20:25:39 +0200

    newWeb

---
+++ personal.js
@@ -0,0 +1,87 @@
+const body = document.querySelector("body")
+const devDiv = document.querySelector("#developerDiv")
+const adminDiv = document.querySelector("#designerDiv")
+const fotoPro = document.querySelector("#proFoto")
+const profileTop = document.querySelector("#top")
+const title = document.querySelector("#title")
+const mario = document.querySelector("#marioPg")
+const sky = document.querySelector("#sky")
+const star = document.querySelector("#star")
+devDiv.addEventListener("mouseenter", function(){
+    body.style.backgroundImage = "url('images/coding.jpeg')"
+    fotoPro.src = "images/cod.jpeg"
+    title.style.color = "white"
+})
+
+adminDiv.addEventListener("mouseenter", function(){
+    body.style.backgroundImage = "url('images/admin.jpeg')"
+    fotoPro.src = "images/ad.jpeg"
+    title.style.color = "white"
+    
+})
+
+body.addEventListener("mouseenter", function(){
+    body.style.backgroundImage = "url('images/nature.jpeg')"
+    fotoPro.src = "images/normal.jpeg"
+    title.style.color = "black"
+})
+
+
+profileTop.addEventListener("mouseover",function(){
+    body.style.backgroundImage = "url('images/nature.jpeg')"
+    fotoPro.src = "images/normal.jpeg"
+    title.style.color = "black"
+})
+
+let marginLeftPg = 0
+body.addEventListener("keydown", function(e){
+    console.log(e.key)
+    switch (e.key) {
+        case "d":
+            marginLeftPg = marginLeftPg + 50
+            if (marginLeftPg > (sky.offsetWidth -50) ){
+                marginLeftPg = sky.offsetWidth - 50
+                mario.style.marginLeft = `${marginLeftPg}px`
+            break;
+            } else {
+                mario.style.marginLeft = `${marginLeftPg}px`
+                break;
+            }
+            
+        case "a":
+                marginLeftPg = marginLeftPg - 50
+                if (marginLeftPg < 0){
+                    marginLeftPg = 0
+                    mario.style.marginLeft = `0px`
+                     break;
+                }else {
+                    mario.style.marginLeft = `${marginLeftPg}px`
+                break;
+                }
+                
+        case "w":
+                mario.style.marginTop = "50px"
+                setTimeout(()=>{
+                    mario.style.marginTop = "100px"
+                },500)
+                break;
+        
+        default:
+            break;
+    }
+})
+
+function stars(){
+    const positionW = Math.floor(Math.random()*sky.offsetWidth)
+    const positionH = (Math.floor(Math.random()*100))+50
+    star.style.marginLeft = `${positionW}px`
+    star.style.marginTop = `${positionH}px`
+    star.style.display = "inline"
+}
+
+setInterval(() => {
+    stars()
+}, 2000);
+
+
+
